6957a1e5af530c169ddad064dad90660cf964b60
RYOTA TAJIMA <tajima1026@gmail.com>
2019-03-16 01:07:36 +0900

    Delete core/plugin in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,8 +23,6 @@
 //= require material/arrive.min
 //= require material/material-dashboard
 //= require moment.min
-//= require core
-//= require plugins
 //= require jquery_ujs
 //= require fullcalendar.min
 //= require_tree .
